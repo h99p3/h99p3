b42a9081f3b1aefe56f1c235159c5f05d2d58e43
h99p3 <h99p3@loskiminos.club>
2016-08-27 15:01:28 +0200

    adding 1 working test for 1elt list, and 1 nonworking test for empty list

--- H99p01_test.hs
+++ H99p01_test.hs
@@ -8,11 +8,17 @@ test_char = TestCase (assertEqual "testing last elem from a list of char"
                        (last' ['g','3','n','r']) (Just 'r'))
 test_bool = TestCase (assertEqual "testing last elem from a list of bool"
                        (last' [True, True, False]) (Just False))
+test_1elt = TestCase (assertEqual "testing last elem from a list of length 1"
+                       (last' [5]) (Just 5))
+--test_emptylist = TestCase (assertEqual "testing last elem from an empty list"
+                       --(last' []) Nothing)
 
 tests = TestList [
                    TestLabel "test_int" test_int
                  , TestLabel "test_char" test_char
                  , TestLabel "test_bool" test_bool
+                 , TestLabel "test_1elt" test_1elt
+                 --, TestLabel "test_emptylist" test_emptylist
                  ]
 
 main = runTestTT tests
