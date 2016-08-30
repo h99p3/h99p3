ff54adb74850f5f7437842193565b892b3e9a9a4
h99p3 <h99p3@loskiminos.club>
2016-08-31 01:49:11 +0200

    simplifying code

--- H99p02.hs
+++ H99p02.hs
@@ -5,9 +5,9 @@ module H99p02
 
 --Find the last but one element of a list.
 h99p02 :: [a] -> Maybe a
-h99p02 [] = Nothing
-h99p02 [_] = Nothing
-h99p02 xs = Just (sndElt (reverse xs))
-   where sndElt (x:y:ys) = y
+h99p02 xs = if length xs < 2
+            then Nothing
+            else Just (sndElt (reverse xs))
+                   where sndElt (x:y:ys) = y
 
 
